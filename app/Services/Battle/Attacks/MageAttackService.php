2247fc9bd7432c652e7057e4ef8b00728f010fe9
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2026-06-02 13:52:21 +0200

    Update MageAttackService.php

--- app/Services/Battle/Attacks/MageAttackService.php
+++ app/Services/Battle/Attacks/MageAttackService.php
@@ -13,77 +13,30 @@ class MageAttackService
     ): array {
         return match ($attack) {
 
-            'fireball' => $this->fireball(
-                $attacker,
-                $defender
-            ),
+            'fireball' => [
+                'damage' => rand(20, 35),
+                'message' => "{$attacker->user->name} slingert een fireball.",
+            ],
 
-            'frost_nova' => $this->frostNova(
-                $attacker,
-                $defender
-            ),
+            'frost_nova' => [
+                'damage' => rand(10, 20),
+                'skip_turn' => true,
+                'message' => "{$attacker->user->name} gebruikt frost nova.",
+            ],
 
-            'arcane_blast' => $this->arcaneBlast(
-                $attacker,
-                $defender
-            ),
-            'you_shall_not_pass' => $this->youShallNotPass(
-                $attacker,
-                $defender
-            ),
+            'arcane_blast' => [
+                'damage' => rand(15, 45),
+                'message' => "{$attacker->user->name} gebruikt arcane blast.",
+            ],
+
+            'you_shall_not_pass' => [
+                'damage' => rand(900, 1500),
+                'message' => "{$attacker->user->name} roept: YOU SHALL NOT PASS!",
+            ],
 
             default => [
                 'error' => 'Attack bestaat niet.',
-            ]
+            ],
         };
-    }
-
-    private function fireball(
-        GamePlayer $attacker,
-        GamePlayer $defender
-    ): array {
-        $damage = rand(20, 35);
-
-        return [
-            'damage' => $damage,
-            'message' => "{$attacker->user->name} slingert een fireball.",
-        ];
-    }
-
-    private function frostNova(
-        GamePlayer $attacker,
-        GamePlayer $defender
-    ): array {
-        $damage = rand(10, 20);
-
-        return [
-            'damage' => $damage,
-            'skip_turn' => true,
-            'message' => "{$attacker->user->name} gebruikt frost nova.",
-        ];
-    }
-
-    private function arcaneBlast(
-        GamePlayer $attacker,
-        GamePlayer $defender
-    ): array {
-        $damage = rand(15, 45);
-
-        return [
-            'damage' => $damage,
-            'message' => "{$attacker->user->name} gebruikt arcane blast.",
-        ];
-    }
-
-    private function youShallNotPass(
-        GamePlayer $attacker,
-        GamePlayer $defender
-    ): array {
-        $damage = rand(900, 1500);
-
-        return [
-            'damage' => $damage,
-            'message' => "{$attacker->user->name} You shall not pass!",
-        ];
     }
 }
